8b408335a5540518c0b88c6529d1069cf76eeda2
Farhan Sadeek <farhansadeek019@gmail.com>
2024-07-11 08:30:42 -0400

    initial commit

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,26 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    title: "Dual Enrollment",
+    duration: "2023 - Present",
+    institution: "Massachusetts Institute of Technology",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
+    title: "Dual Enrollment",
     duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    institution: "Stanford University",
   },
   {
     id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
-  }
+    title: "Dual Enrollment",
+    duration: "2023 - Present",
+    institution: "The Ohio State University",
+  },
+  {
+    id: 4,
+    title: "Dual Enrollment",
+    duration: "2023 - Present",
+    institution: "The Ohio State University",
+  },
 ]
